8fbad17f6f134dc97ea5801d33d87f477d64f197
sivaprakasam <sivaprakasam.s@sage.com>
2013-04-22 12:43:47 +0530

    New sample project added
    Verified

---
+++ UnityApplicationBlock/UnityApplicationBlock/FileWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web;
+
+namespace UnityApplicationBlock 
+{
+    public class FileWriter : IWriter
+    {
+
+        public void Write(string msg)
+        {
+             
+
+            using (StreamWriter streamWriter =
+                new StreamWriter("d:\\TestUnity.txt", false))
+            {
+                streamWriter.WriteLine(msg);
+             
+            }
+        }
+    }
+}
---
+++ UnityApplicationBlock/UnityApplicationBlock/WriterHelperCls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace UnityApplicationBlock
+{
+    public class WriterHelperCls
+    {
+        private IWriter selectWriter;
+
+        [Dependency]
+        public IWriter SelectedWriter
+        {
+            get { return selectWriter; }
+            set { selectWriter = value; }
+        }
+
+        public void WriteOutput(string msg)
+        {
+            selectWriter.Write(msg);
+        }
+    }
+}
